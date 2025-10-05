5d9f25ae0f936f0e06999ccba9695dae4bf08fd8
Jack Punch <414692854@qq.com>
2025-10-05 04:03:18 +0000

    feat: 添加用户交互功能
    - 新增图片点击切换功能
    - 添加用户名称设置按钮及本地存储功能
    - 更新样式以匹配新功能

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,38 @@ function toggleDone(e) {
 listItems.forEach((item) => {
   item.addEventListener("click", toggleDone);
 });
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/flower.jpg") {
+    myImage.setAttribute("src", "images/flower2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/flower.jpg");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
